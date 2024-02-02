d40f20b6ac6d54485a54add5bfc33c3e4e151cfd
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2024-02-02 13:13:36 +0300

    feat: full working bot

--- backend/websocket/src/index.ts
+++ backend/websocket/src/index.ts
@@ -1,1 +1,122 @@
-console.log('Hello World from Toxic_Cat')
+const tmi = require('tmi.js')
+const fs = require('fs')
+
+// Read ENV variables
+type configType = {
+	USERNAME: string
+	PASSWORD: string
+	CHANNEL: string
+	TIMEOUT_MIN: number
+	TIMEOUT_MAX: number
+}
+
+let config: configType = {
+	USERNAME: '',
+	PASSWORD: '',
+	CHANNEL: '',
+	TIMEOUT_MIN: 0,
+	TIMEOUT_MAX: 0,
+}
+
+if (!process.env.USERNAME) {
+	console.log('Error: Config: USERNAME not set')
+	process.exit()
+} else {
+	config.USERNAME = process.env.USERNAME
+}
+
+if (!process.env.PASSWORD) {
+	console.log('Error: Config: PASSWORD not set')
+	process.exit()
+} else {
+	config.PASSWORD = process.env.PASSWORD
+}
+
+if (!process.env.CHANNEL) {
+	console.log('Error: Config: CHANNEL not set')
+	process.exit()
+} else {
+	config.CHANNEL = process.env.CHANNEL
+}
+
+if (!process.env.TIMEOUT_MIN) {
+	console.log('Error: Config: TIMEOUT_MIN not set')
+	process.exit()
+} else {
+	config.TIMEOUT_MIN = parseInt(process.env.TIMEOUT_MIN)
+}
+
+if (!process.env.TIMEOUT_MAX) {
+	console.log('Error: Config: TIMEOUT_MAX not set')
+	process.exit()
+} else {
+	config.TIMEOUT_MAX = parseInt(process.env.TIMEOUT_MAX)
+}
+
+// Read strings from text file
+
+const messages = fs.readFileSync('./result.txt', 'utf8').split('\n')
+
+const client = new tmi.Client({
+	options: { debug: false },
+	connection: {
+		secure: true,
+		reconnect: true,
+	},
+	identity: {
+		username: config.USERNAME,
+		password: 'oauth:' + config.PASSWORD,
+	},
+	channels: [config.CHANNEL],
+})
+
+client.connect()
+
+// client.on('message', (channel: any, tags: any, message: any, self: any) => {
+// Ignore echoed messages.
+// if (self) return
+
+// Don't reply to commands
+// if (message.startsWith('!')) return
+
+// Ignore if message is from the other bot.
+
+// Ignore if timeout is active.
+
+// Random message
+
+// let randomIndex = Math.floor(Math.random() * messages.length)
+
+// console.log(randomIndex)
+
+// if (message.toLowerCase() === '!hello') {
+// 	client.say(channel, messages[randomIndex])
+// }
+// })
+
+const setRandomInterval = (minDelay: any, maxDelay: any) => {
+	let timeout: any
+
+	const runInterval = () => {
+		const timeoutFunction = () => {
+			let randomIndex = Math.floor(Math.random() * messages.length)
+			client.say(config.CHANNEL, messages[randomIndex])
+			runInterval()
+		}
+
+		const delay =
+			Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay
+
+		timeout = setTimeout(timeoutFunction, delay)
+	}
+
+	runInterval()
+
+	return {
+		clear() {
+			clearTimeout(timeout)
+		},
+	}
+}
+
+setRandomInterval(config.TIMEOUT_MIN * 1000, config.TIMEOUT_MAX * 1000)
